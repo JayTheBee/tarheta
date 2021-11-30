41f29cb19e95f74b8e9a77eabf3986fcc920e5dd
Jediboy Betos <jediboy.betos811@gmail.com>
2021-11-30 17:58:44 +0800

    Refactoring and custom library

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -2,6 +2,18 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Auth extends CI_Controller{
+
+
+	//jediboy: Added class constructs
+	public function __construct(){
+		parent::__construct();
+		$this->load->library('email');
+		$this->load->library('form_validation');
+		$this->load->helper('security');
+		$this->load->helper('security');
+		$this->load->model('user_model');
+	}
+
 	function view($page = 'home'){
 		if(!file_exists(APPPATH.'views/pages/'.$page.'.php')){
 			show_404();
@@ -13,62 +25,72 @@ class Auth extends CI_Controller{
 		$this->load->view('pages/'.$page, $data);
 		$this->load->view('templates/footer');
 	}
-
-	function signup(){
-
+	//jediboy:refactored
+	function captcha() {
 		if (($_SERVER['REQUEST_METHOD']=='POST' && $_POST['g-recaptcha-response'] != "")){
 			$secret = env('RCAPTCHA_SECRET_KEY');// Secret key. Nasakin ung keys. si ramon kasi ung sa .env - ryle
 			$verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret=' . $secret . '&response=' . $_POST['g-recaptcha-response']);
-			$responseData = json_decode($verifyResponse);
+			return $responseData = json_decode($verifyResponse);
+		}
+	}
+	//jediboy: refactored
+	function signup_clean(){
+		$data2 = array(
+			'type' => $_SESSION['usertype'],
+		);
 
-			if ($responseData->success) {
-				$this->load->library('form_validation');
+		$username = $this->input->post('username', TRUE);
+		$email = $this->input->post('email', TRUE);
+		$password = $this->input->post('password', TRUE);
+
+		$code = bin2hex(openssl_random_pseudo_bytes(10)); // Jedi okay na ba tong pang generate ng active_token or may better way ba?
+		$code2 = bin2hex(openssl_random_pseudo_bytes(10));
+
+		$data = array (
+			'username'=>$username,
+			'email'=>$email,
+			'password'=>password_hash($password, PASSWORD_DEFAULT),
+			'active_token' => $code,
+			'reset_token' => $code2,
+		);
+
+		$this->user_model->insertuser($data, $data2);
+		$this->email_verify($username, $code, $email);
+	}
+	//jediboy:refactored
+	function email_verify($username, $code, $email){
+
+		$this->session->set_flashdata('success','Successfully Created. You can now login.');
+		$mail = array(
+			'subject' => "Tarheta | Activeate Account",
+			'header' => "Activate your account",
+			'username' => $username,
+			'body' => "Please click the the button to activate your account",
+			'button' => "Activate",
+			'link' => base_url()."auth/verify/".$username."/".$code,
+		);
+
+		$this->email->send_email($mail, 'templates/email', $email);
+		redirect(base_url('login'));
+	}	
+	//jediboy:refactored
+
+	function signup(){
+
+		if ($this->captcha()->success) {
+				
 				$this->form_validation->set_rules('username','Username','required|is_unique[users.username]'); //<- is_unique[dbTableName.FieldToBeChecked]
 				$this->form_validation->set_rules('email','Email','required|is_unique[users.email]|valid_email');
 				$this->form_validation->set_rules('password','Password','required');
 				$this->form_validation->set_rules('confirm_password','Confrim Password','required|matches[password]');
 
 				if($this->form_validation->run()==TRUE){
-					$this->load->helper('security');
-					$data2 = array(
-						'type' => $_SESSION['usertype'],
-					);
-
-					$username = $this->input->post('username', TRUE);
-					$email = $this->input->post('email', TRUE);
-					$password = $this->input->post('password', TRUE);
-					$code = bin2hex(openssl_random_pseudo_bytes(10)); // Jedi okay na ba tong pang generate ng active_token or may better way ba?
-					$code2 = bin2hex(openssl_random_pseudo_bytes(10));
-
-					$data = array (
-						'username'=>$username,
-						'email'=>$email,
-						'password'=>password_hash($password, PASSWORD_DEFAULT),
-						'active_token' => $code,
-						'reset_token' => $code2,
-					);
-
-					$this->load->model('user_model');
-					$this->user_model->insertuser($data, $data2);
-					$this->session->set_flashdata('success','Successfully Created. You can now login.');
-
-					$mail = array(
-						'subject' => "Tarheta | Activeate Account",
-						'header' => "Activate your account",
-						'username' => $username,
-						'body' => "Please click the the button to activate your account",
-						'button' => "Activate",
-						'link' => base_url()."auth/verify/".$username."/".$code,
-					);
-					$this->sendEmail($mail, 'templates/email', $email);
-
-					redirect(base_url('login'));
+					$this->signup_clean();
 				}
 				$this->view('signup');
 				// $this->load->view('templates/header');
 				// $this->load->view('pages/signup');
 				// $this->load->view('templates/footer');
-			}
 		}
 		else{
 			$this->session->set_flashdata('error','reCaptcha is Required.');
@@ -84,7 +106,7 @@ class Auth extends CI_Controller{
 		return $segmentedURL;
 	}
 
-
+	//jediboy: refactored
 	function verify(){
 		$url = $this->segmentURL();
 		$data = array(
@@ -92,7 +114,7 @@ class Auth extends CI_Controller{
 			'active_timestamp' => date('Y/m/d h:i:s'), // To be Improved. Issue mali pa ung time pero okay ung date.
 		);
 
-		$this->load->model('user_model');
+		
 		$query = $this->user_model->verifyAccount($data, $url['username'], $url['code']);
 		if($query){
 			$this->view('verified');
@@ -113,6 +135,24 @@ class Auth extends CI_Controller{
 		redirect(base_url('signup'));
 	}
 	
+	//jediboy: refactored
+	function login_redirect($status, $username, $email, $password){
+		$username = $status->username;
+		$email = $status->email;
+		$session_data = array(
+			'username'=>$username,
+			'email'=>$email,
+			'password'=>password_hash($password, PASSWORD_DEFAULT),
+		);
+
+		$query = $this->user_model->getProfile($email);
+		$profile = (array) $query; //Typecasting from object to array
+		$this->session->set_userdata('Profile',$profile);
+		$this->session->set_userdata('UserLoginSession',$session_data);
+		
+		redirect(base_url('profile'));
+	}
+
 
 	function login(){
 		if($_SERVER['REQUEST_METHOD']=='POST'){
@@ -123,25 +163,10 @@ class Auth extends CI_Controller{
 				$email = $this->input->post('email', TRUE);
 				$username = $this->input->post('username', TRUE);
 				$password = $this->input->post('password',TRUE);
-
-				$this->load->model('user_model');
-				
 				$status = $this->user_model->passCheck($password,$email);
-				if($status!=false){
-					$username = $status->username;
-					$email = $status->email;
-					$session_data = array(
-						'username'=>$username,
-						'email'=>$email,
-						'password'=>password_hash($password, PASSWORD_DEFAULT),
-					);
 
-					$query = $this->user_model->getProfile($email);
-					$profile = (array) $query; //Typecasting from object to array
-					$this->session->set_userdata('Profile',$profile);
-					$this->session->set_userdata('UserLoginSession',$session_data);
-					
-					redirect(base_url('profile'));
+				if($status!=false){
+					$this->login_redirect($status, $username, $email, $password);
 				}
 				else{
 					$this->session->set_flashdata('error', 'Incorrect Email or Password');
@@ -173,10 +198,10 @@ class Auth extends CI_Controller{
 		$this->form_validation->set_rules('confirm_password','Confrim Password','required|matches[password]');
 
 		if($this->form_validation->run()==TRUE){
-			$this->load->helper('security');
+
 			$password = password_hash($this->input->post('password', TRUE), PASSWORD_DEFAULT);
 
-			$this->load->model('user_model');
+	
 			$this->user_model->updatePassword($_SESSION['resetpassword'], $password);
 
 			$this->session->set_flashdata('success','Password changed successfully.');
@@ -196,7 +221,7 @@ class Auth extends CI_Controller{
 	/* Function to extract the code and username in the reset password link and sends it to the model */
 	function resetPassCheck(){
 		$data = $this->segmentURL();
-		$this->load->model('user_model');
+
 		$query = $this->user_model->codeCheck($data['username'], $data['code']);
 		
 		if($query){
@@ -218,12 +243,12 @@ class Auth extends CI_Controller{
 		if($_SERVER['REQUEST_METHOD']=='POST')
 		{
 			
-			$this->load->library('form_validation');
+
 			$this->form_validation->set_rules('email','Email','required|valid_email');
 			if($this->form_validation->run()==TRUE){
 				$email = $this->input->post('email');
 
-				$this->load->model('user_model');
+
 				
 				$status = $this->user_model->emailCheck($email);
 
@@ -240,8 +265,8 @@ class Auth extends CI_Controller{
 						'button' => "Reset",
 						'link' => base_url()."auth/resetPassCheck/".$status->{'username'}."/".$status->{'reset_token'},
 					);
-					
-					$this->sendEmail($data, 'templates/email', $email);
+
+					$this->email->send_email($data, 'templates/email', $email);
 
 					$this->session->set_flashdata('success','Password reset email sent.');
 					redirect(base_url('login'));
@@ -259,40 +284,41 @@ class Auth extends CI_Controller{
 		}
 	}
 
+	//Jediboy: Added a MY_email custom library instead
 	/*
 		* I decided to also pass in the email template maybe we can use this function to send another 
 			email but with a different template. ie Email when you joined a class or something.
 	*/
-	function sendEmail($data, $emailTemplate, $email){
-		/*
-	 		* Changed the email sending. It now builds on this: https://www.youtube.com/watch?v=ctUUxx0Klng
-			 	but it loads a php file rather than a string.
-		*/
-		$to = $email;
-		$subject = $data['subject'];
-		$from = env('EMAIL');
+	// function sendEmail($data, $emailTemplate, $email){
+	// 	/*
+	//  		* Changed the email sending. It now builds on this: https://www.youtube.com/watch?v=ctUUxx0Klng
+	// 		 	but it loads a php file rather than a string.
+	// 	*/
+	// 	$to = $email;
+	// 	$subject = $data['subject'];
+	// 	$from = env('EMAIL');
 
-		$config['protocol'] = 'smtp';
-		$config['smtp_host'] = 'ssl://smtp.gmail.com';
-		$config['smtp_port'] = '465';
-		$config['smtp_timeout'] = '60';
+	// 	$config['protocol'] = 'smtp';
+	// 	$config['smtp_host'] = 'ssl://smtp.gmail.com';
+	// 	$config['smtp_port'] = '465';
+	// 	$config['smtp_timeout'] = '60';
 		
-		$config['smtp_user'] = env('EMAIL');
-		$config['smtp_pass'] = env('EMAIL_PASSWORD');
+	// 	$config['smtp_user'] = env('EMAIL');
+	// 	$config['smtp_pass'] = env('EMAIL_PASSWORD');
 		
-		$config['charset'] = 'utf-8';
-		$config['newline'] = "\r\n";
-		$config['mailtype'] = 'html';
-		$config['validation'] = TRUE;
+	// 	$config['charset'] = 'utf-8';
+	// 	$config['newline'] = "\r\n";
+	// 	$config['mailtype'] = 'html';
+	// 	$config['validation'] = TRUE;
 
-		$this->email->initialize($config);
-		$this->email->set_mailtype("html");
-		$this->email->from($from);
-		$this->email->to($to);
-		$this->email->subject($subject);
+	// 	$this->email->initialize($config);
+	// 	$this->email->set_mailtype("html");
+	// 	$this->email->from($from);
+	// 	$this->email->to($to);
+	// 	$this->email->subject($subject);
 
-		$this->email->message($this->load->view($emailTemplate,$data,true));
-		$this->email->send();
-	}
+	// 	$this->email->message($this->load->view($emailTemplate,$data,true));
+	// 	$this->email->send();
+	// }
 }
 ?>
