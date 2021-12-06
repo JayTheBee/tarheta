18d680ccd1799c6e5b3a8aad8a5e39f0b06479c9
Jediboy Betos <jediboy.betos811@gmail.com>
2021-12-06 12:05:06 +0800

    Updated PHPUnit vendor files. Added more tests

--- application/tests/controllers/Pages_test.php
+++ application/tests/controllers/Pages_test.php
@@ -1,8 +1,19 @@
 <?php
-
+/**
+ * @group controller
+ * 
+ **/
 class Pages_test extends TestCase {
 	public function test_home(){
 		$output = $this->request('GET', '/');
-		$this->assertContains('Home Page', $output);
+		$this->assertContains('<div class="card-header text-center"> Home Page </div>', $output);
 	}
+	public function test_404(){
+		$this->request('GET', 'notfound');
+		$this->assertResponseCode(404);
+	}
+	// public function test_signup(){
+	// 	$output = $this->request('GET', 'signup');
+	// 	$this->assertContains('Register as a', $output);
+	// }
 }
