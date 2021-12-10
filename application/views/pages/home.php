792e361aeab2dd1330863b14e14c77d1b1474696
loryvi <lilyleogo@gmail.com>
2021-12-10 13:56:45 +0800

    test push

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,5 +1,8 @@
-
-    <div class="container">
+<div class="homepage container">
+<p class="text-center">
+</div>  
+  
+<div class="container">
         <div class="row">
             <div class="col-md-4"></div>
             <div class="col-md-4">
@@ -8,7 +11,7 @@
 
                 <div class="card-body">
 
-                    <p>HELLO WORLD </p>
+                    <p>HELLO WORLD hatdoooasdsddsg  test lang sa push</p>
                     <?php 
                         if(isset($_SESSION['UserLoginSession'])){
                             echo "<pre>";
