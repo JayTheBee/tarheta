b2907f96d0317a7dfb745cf3623516bc9bc1c1b1
loryvi <lilyleogo@gmail.com>
2021-12-12 15:25:33 +0800

    push features homepage

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,13 +4,17 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <!--Css-->
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/home.css"); ?>">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     
     <!-- Lagay ko na ito dito pero sa login ko palang need para sa forgot pass modal popup -->
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+    <style>
+      @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap");
+    </style>
 
     <!-- my css -->
     <link rel="stylesheet" href="<?php echo base_url("assets/css/style.css"); ?>">
@@ -30,10 +34,9 @@
               <div class="collapse navbar-collapse show" id="navbarCollapse">
                   <ul class="navbar-nav flex-row flex-wrap ms-auto pt-2 py-md-0">
                       <li class="nav-item col-6 col-md-auto px-lg-5 px-3"><a class="nav-link" href="<?php echo base_url();?>home">HOME</a></li>
-                      <li class="nav-item col-6 col-md-auto px-lg-5 px-3"><a class="nav-link" href="#">FEATURES</a></li>
+                      <li class="nav-item col-6 col-md-auto px-lg-5 px-3"><a class="nav-link" href="<?php echo base_url();?>features">FEATURES</a></li>
                       <li class="nav-item col-6 col-md-auto px-lg-5 px-3"><a class="nav-link" href="#">DEVELOPERS</a></li>
                   </ul>
-
                   <div class="ms-auto">
                       <a class="btn btn-bd-login rounded-pill text-decoration-underline" href="#" role="button" href="#" role="button">LOG IN</a>
                   </div>
