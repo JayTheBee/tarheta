4bbf660124eb0230d059f2367a33286033ed3f3a
Kenblader Nonan <kenshiromonterde@gmail.com>
2021-12-12 14:38:49 +0800

    push home.php

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,31 +1,33 @@
+<!--home-->
+    <section class="home-bg">
+        <div class="min-vh-100 mx-5">
+            <img src="assets/images/home/home_bg.png" class="float-left position-absolute wave-size">
+            <div class="row positionZ">
+                <div class="col-6 hText-padding-top px-lg-4">
+                    <div class="d-flex align-content-center flex-column ">
+                        <div class="home-text">
+                        <h1>Study Anywhere, Anytime & Grow Your Skills</h1>
+                        </div>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4"></div>
-            <div class="col-md-4">
-            <div class="card" style="margin-top: 5rem">
-                <div class="card-header text-center"> Home Page </div>
-
-                <div class="card-body">
-
-                    <p>HELLO WORLD </p>
-                    <?php 
-                        if(isset($_SESSION['UserLoginSession'])){
-                            echo "<pre>";
-                            print_r($_SESSION['UserLoginSession']);
-                            echo "</pre>";
-
-                            echo "<pre>";
-                            print_r($_SESSION['Profile']);
-                            echo "</pre>";
-                        }
-                        // Pang check ko lang ito ng current SESSION DATA
-                    ?>
-
-
+                        <div> <!--just to make the get started button not occupying the full width-->
+                            <a class="btn btn-bd-getStarted rounded-pill" href="#" role="button">Get Started ></a> 
+                        </div>
+                    </div>
+                </div>
+            
+                <div class="col-6">
+                    <div class="img-size-girl my-5 py-lg-2">
+                        <img src="<?php echo base_url("assets/images/home/home-girl.png"); ?>">
+                    </div>
                 </div>
             </div>
+
+            <!--Not entirely responsive-->
+            <div class="img-size-plant">
+                <img src="assets/images/home/home-plant.png">
             </div>
-            <div class="col-md-4"></div>
         </div>
-    </div>
+    </section>
+
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
