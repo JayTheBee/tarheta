36e0ceb8d1ccb31fd1a15cef7404df962ec3bbc8
Alliyah Gonzales <allyxgnzls@gmail.com>
2021-12-12 15:45:30 +0800

    uploaded developers page

---
+++ application/views/pages/developers.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/style.css"); ?>">
+    <title>Tarheta</title>
+</head>
+
+<body>
+    <section id="Developers" class="dev-custom">
+        <div class="bg-custom">
+            <div class="product-holder">
+                <img src="<?php echo base_url("assets/images/Vector 14.png"); ?>" class="float-left image-custom " alt="...">
+                <img src="<?php echo base_url("assets/images/rectangle.png"); ?>" class="position-absolute top-50 start-50 translate-middle rectangle" alt="...">
+                <img src="<?php echo base_url("assets/images/molecule.png"); ?>" class="position-absolute top-0 start-0 molecule" alt="...">
+                <img src="<?php echo base_url("assets/images/friends.png"); ?>" class="position-absolute bottom-0 end-0 friends" alt="...">
+            </div>
+        </div>
+
+        <footer class="footer footer-custom" id="footer">
+            <div class="container-lg py-4">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <p class="footer-text m-0 text-center">&copy; 2021 TEAM02 BSCS3CD</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </section>
+
+    <!-- footer starts here -->
+
+    <!-- footer ends here -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
