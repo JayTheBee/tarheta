a7418f3d3cd8b8d9e0f0f997293c8774ad57d816
Jediboy Betos <jediboy.betos811@gmail.com>
2022-01-07 00:56:15 +0800

    Added 'Only Teachers can create classes' prototype

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -39,9 +39,11 @@
                       <li class="nav-item">
                         <a class="nav-link" href="<?php echo base_url(); ?>classes/join">Join Class</a>
                       </li>
+        <?php if($_SESSION['UserType']['type'] == 'TEACHER') : ?>
                       <li class="nav-item">
                         <a class="nav-link" href="<?php echo base_url(); ?>classes/create">+ Create Class</a>
                       </li>
+        <?php endif; ?>
                       <li class="nav-item">
                         <a class="nav-link" href="<?php echo base_url(); ?>flashcards/create">+Create Flashcards</a>
                       </li>
