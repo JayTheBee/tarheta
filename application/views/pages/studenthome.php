e8a217f21e356866a07efa7f7e06b228578c1561
jon-velasco <91194481+jon-velasco@users.noreply.github.com>
2022-02-07 14:11:34 +0800

    studenthome

---
+++ application/views/pages/studenthome.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<section class="teacherStudent-bg">
+    <div>
+        <img src="<?php echo base_url("assets/images/student/Vector 16.png");?>" class="waveSize position-absolute" style="padding-top : 50px;" alt="wave">        
+        
+
+
+        <div class="position-relative boxPos d-flex justify-content-center ">
+            <div class="boxWidth">
+                <div class="container-fluid secondBox">
+                 <img src="<?php echo base_url("assets/images/student/student1.png"); ?>" class="position-absolute teacherStudent-boy1" alt="boy">   
+                 <h3>Recent Sets</h3>
+                    <p>You don't have any sets yet</p>
+                    <a>Sets you create or study will be displayed here. If you want to create a new set click<a href="">here</a></a><br>
+                    <h3>Classes</h3> 
+                    <div class="buttonIn">
+        <input placeholder="Enter Code" type="text" id="enter">
+        <button id="clear">Join</button>
+        <h3 style="padding-top: 30px;">See Other Users Flash Cards</h3>
+    <div class="f2">
+        <img src="<?php echo base_url("assets/images/student/Group10.png");?>" class="" alt="wave">
+    </div>
+    <div class="f2">
+        <img src="<?php echo base_url("assets/images/student/Group9.png");?>" class="" alt="wave">
+    </div>
+</div>
+
+
+
+
+
+
+        
+    </div>
+</div>
+    
+  </div>
+</nav>
+                </div>
+            </div>
+        </div>
+    </div> 
+   </form>
+</div>
+</section>
+
+
+
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800&display=swap');
+    </style>
+   
+
+    <title>Sprint Demo</title>
+</head>
+ 
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light fixed-top">
+        <div class="container">
+            <div width="1" class="create-nav design">
+                <div width="0.525" class="nav design">
+                    <span class="navbar-brand brand-des mb-0 h1">LOGO</span> <!-- will eventually change to picture -->
+                    <button aria-label="Enter tarheta title..." class="nav-button">
+                        <span class="text">Enter tarheta title…</span>
+                        <span class="settings-button mh-75 d-inline-block">
+                            <span class="settings">Settings</span>
+                        </span>
+                    </button>
+                </div>
+                <div width="0.475" class="right-nav design">
+                    <button type="button" id="topbar-preview-button" class="buttons preview">
+                        <i class="fas fa-eye"></i>
+                        <span>&nbsp;Preview</span>
+                    </button>
+
+                    <div class="line">
+                        <div class="line-between">
+                        </div>
+                    </div>
+
+                    <button type="button" class="buttons exit-button" data-functional-selector="top-bar__exit-button">
+                        Exit
+                    </button>
+
+                    <button type="approve" class="buttons save-button" data-functional-selector="top-bar__save-button">
+                        Save
+                    </button>
+                </div>
+            </div>
+
+
+        </div>
+   
+
+                
+                   
+               
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    
+
+   
+
+            
